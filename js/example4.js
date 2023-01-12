e59fa2ea81cd030b0eb27053a1bc759c33dbed3a
kznDan <danuafrolov@gmail.com>
2023-01-12 15:20:20 +0400

    1

--- js/example4.js
+++ js/example4.js
@@ -40,5 +40,24 @@ delete user.name;*/
   }*/
 
 
+  let menu = {
+    width: 200,
+    height: 300,
+    title: "My menu"
+  };
   
+  multiplyNumeric(menu);
   
+  menu = {
+    width: 400,
+    height: 600,
+    title: "My menu"
+  };
+  
+  function multiplyNumeric(obj) {
+    for (let key in obj) {
+      if (typeof obj[key] == 'number') {
+        obj[key] *= 2;
+      }
+    }
+  }
