04241a97319f7fc25c49eb30a2ad1945d0e5a0dc
kznDan <danuafrolov@gmail.com>
2023-01-25 21:02:37 +0400

    Update example5.js

---
+++ js/example5.js
@@ -0,0 +1,21 @@
+/*let a = +prompt("Введите первое число", "");
+let b = +prompt("Введите второе число", "");
+
+alert( a + b );*/
+
+
+
+function readNumber() {
+    let num;
+  
+    do {
+      num = prompt("Введите число", 0);
+    } while ( !isFinite(num) );
+  
+    if (num === null || num === '') return null;
+  
+    return +num;
+  }
+  
+  alert(`Число: ${readNumber()}`);
+  
