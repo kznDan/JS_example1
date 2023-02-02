838c6f6ceaa5045f75e145316a3a0d6d3e4edb41
kznDan <danuafrolov@gmail.com>
2023-02-02 19:47:00 +0400

    1

---
+++ js/exampleCodewars.js
@@ -0,0 +1,34 @@
+/*1. 'world'  =>  'dlrow'
+'word'   =>  'drow'
+
+
+function solution(str){
+  let word = ''
+  for (let i of str){
+    word = i + word
+    
+  }
+  return word
+}
+*/
+
+/*2.Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+function boolToWord( bool ){
+  return bool ? 'Yes': 'No'
+}
+*/
+/*3.Write a function that takes an array of numbers and returns the sum of the numbers. The numbers can be negative or non-integer. If the array does not contain any numbers then you should return 0.
+
+
+function sum (numbers) {
+    "use strict";
+    let acc = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    
+      acc += numbers[i]
+    
+  }
+  return acc;
+}
+*/
