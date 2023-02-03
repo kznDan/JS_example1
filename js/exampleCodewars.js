f748b4138adfce5b21c8914bf91a507aef968c76
kznDan <danuafrolov@gmail.com>
2023-02-03 19:09:28 +0400

    Update exampleCodewars.js

--- js/exampleCodewars.js
+++ js/exampleCodewars.js
@@ -32,3 +32,21 @@ function sum (numbers) {
   return acc;
 }
 */
+
+
+/*
+
+4.Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+
+function evenOrOdd(number) {
+    if(number % 2 === 0) {
+      return 'Even';
+    } else {
+      return 'Odd';
+    }
+
+}
+
+*/
+
